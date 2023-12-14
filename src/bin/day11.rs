36cf758b266852851dacb2147137bc7705ea9eb0
Vincent Verhoeven <verhoevenv@gmail.com>
2023-12-14 12:20:46 +0100

    Day 11 part 1

---
+++ src/bin/day11.rs
@@ -0,0 +1,131 @@
+use std::fmt::Display;
+use std::io;
+use std::io::Read;
+
+type REPR = Universe;
+
+#[derive(Debug, Clone)]
+pub struct Universe(Vec<Galaxy>);
+
+impl Universe {
+    pub fn inflate(&self) -> Self {
+        let max_x = self.0.iter().map(|g| g.coordinates.x).max().unwrap();
+        let max_y = self.0.iter().map(|g| g.coordinates.y).max().unwrap();
+
+        let mut new_universe = self.clone();
+
+        for x in 0..max_x {
+            if self.0.iter().all(|g| g.coordinates.x != x) {
+                for g_idx in 0..self.0.len() {
+                    if self.0[g_idx].coordinates.x > x {
+                        new_universe.0[g_idx].coordinates.x += 1;
+                    }
+                }
+            }
+        }
+        for y in 0..max_y {
+            if self.0.iter().all(|g| g.coordinates.y != y) {
+                for g_idx in 0..self.0.len() {
+                    if self.0[g_idx].coordinates.y > y {
+                        new_universe.0[g_idx].coordinates.y += 1;
+                    }
+                }
+            }
+        }
+
+        return new_universe;
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct XY { x: i64, y: i64 }
+
+#[derive(Debug, Clone)]
+pub struct Galaxy {
+    coordinates: XY
+}
+
+impl Galaxy {
+    pub fn distance(&self, other: &Galaxy) -> i64 {
+        (other.coordinates.x - self.coordinates.x).abs() + (other.coordinates.y - self.coordinates.y).abs()
+    }
+}
+
+pub fn compute_1(input: REPR) -> i64 {
+    let inflated = input.inflate();
+    let mut sum = 0;
+    for (g1_idx, g1) in inflated.0.iter().enumerate() {
+        for g2 in &inflated.0[g1_idx..] {
+            sum += g1.distance(g2);
+        }
+    }
+    return sum;
+}
+
+pub fn compute_2(input: REPR) -> i64 {
+    todo!();
+}
+
+pub fn parse(input: &str) -> REPR {
+    let mut result = Vec::new();
+
+    for (row_idx, row) in input.lines().enumerate() {
+        for (col_idx, char) in row.chars().enumerate() {
+            match char {
+                '.' => {},
+                '#' => result.push(Galaxy {coordinates: XY{x: col_idx as i64, y: row_idx as i64}}),
+                _ => panic!("Unknown char: {}", char)
+            }
+        }
+    }
+
+    return Universe(result);
+}
+
+fn main() {
+    read_and_write(parse, &[compute_1, compute_2]);
+}
+
+#[cfg(test)]
+mod tests {
+    use indoc::indoc;
+
+    use super::*;
+
+    const INPUT: &str = indoc! {"
+        ...#......
+        .......#..
+        #.........
+        ..........
+        ......#...
+        .#........
+        .........#
+        ..........
+        .......#..
+        #...#.....
+    "};
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(compute_1(parse(INPUT)), 374);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(compute_2(parse(INPUT)), todo!());
+    }
+}
+
+fn read_and_write<T, S: Display>(parse: fn (&str) -> T, compute: &[fn(T) -> S] ) {
+    let mut input = String::new();
+
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read input");
+
+
+    for f in compute {
+        let result = f(parse(&input));
+        println!("{}", result);    
+    }
+}
